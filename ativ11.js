28a36b2e6b04217cd69da6e506cbaa6d71d6324a
mariaeduardx <123771597+mariaeduardx@users.noreply.github.com>
2023-08-02 11:24:24 -0300

    Add files via upload

---
+++ ativ11.js
@@ -0,0 +1,5 @@
+const entree = 'duda'
+console.log(entree)
+
+/*deu erro, pois uma variável "const" não pode ser reatribuída após a sua declaração inicial.*/
+
---
+++ js.js
@@ -0,0 +1,58 @@
+var favoriteFood = 'pizza'
+
+var numOfSlices = '8'
+
+var favoriteFood = 'pizza'
+console.log(favoriteFood)
+
+var numOfSlices = '8'
+console.log(numOfSlices)
+
+/*let permite que você declare variáveis limitando seu escopo no bloco, instrução, ou em uma expressão na qual ela é usada. Isso é inverso da keyword var (en-US), que define uma variável globalmente ou no escopo inteiro de uma função, independentemente do escopo de bloco.*/
+
+let changeMe = 'true'
+
+changeMe = false
+
+
+ changeMe = 'true'
+console.log(changeMe)
+
+changeMe = false
+console.log(changeMe)
+
+const entree = 'Enchiladas'
+
+entree = 'Enchiladas'
+console.log(entree)
+
+let levelUp = 0
+console.log (++ levelUp)
+
+let powerLevel = 150
+powerLevel -= 100
+console.log (powerLevel)
+
+let multiplyMe =2
+multiplyMe *=11
+console.log (multiplyMe)
+
+let quarterMe =20
+quarterMe /= 4
+console.log (quarterMe)
+
+let gainedDollar 
+console.log(++ gainedDollar)
+
+let lostDollar = 1
+console.log(-- lostDollar)
+
+var Nome = "maria eduarda"
+var city = "Morumbi"
+console.log("meu nome é " +Nome+ "eu moro " + city)
+
+var teste= "maçã"
+console.log (typeof teste)
+
+
+
